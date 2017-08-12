174ca9c826a7cc3ff195fbb3b2b8c2a548cb8fd2
DevAlone <admin@d3d.info>
2017-08-12 22:59:42 +0300

    some fixes to make program compiliable on ubuntu xenial

--- src/parser/pagesparser.cpp
+++ src/parser/pagesparser.cpp
@@ -1,19 +1,18 @@
 // Flamingo is an open-source cross platform program for learning languages
 // Copyright (C) 2017 Sergey Zakharov
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 
 #include "pagesparser.h"
 #include "answersparser.h"
@@ -244,7 +243,7 @@ void PagesParser::parseAnswersSection(std::map<QChar, std::shared_ptr<Answer>>& 
 
                     newAnswerFound = true;
                     answerLetter = letter;
-                    if (trimmedLine > 3)
+                    if (trimmedLine.length() > 3)
                         answerBuffer = trimmedLine.mid(3) + "\n";
                     else
                         answerBuffer = "";
--- src/views/userlistview.cpp
+++ src/views/userlistview.cpp
@@ -1,19 +1,18 @@
 // Flamingo is an open-source cross platform program for learning languages
 // Copyright (C) 2017 Sergey Zakharov
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 
 #include "userlistview.h"
 #include "userlistdispatcher.h"
@@ -61,7 +60,8 @@ void UserListView::showContextMenu(const QPoint& position)
     userContextMenu.addAction(
         QObject::tr("Delete selected user"),
         this,
-        &UserListView::tryToDeleteSelectedItem);
+        SLOT(tryToDeleteSelectedItem()));
+    //&UserListView::tryToDeleteSelectedItem);
 
     userContextMenu.exec(globalPos);
 }
