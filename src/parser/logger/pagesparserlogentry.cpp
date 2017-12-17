e406b4c6ada7d36db2840058b350242f62dbd713
DevAlone <admin@d3d.info>
2017-12-17 16:44:56 +0300

    fixed by pvs-studio

--- src/parser/logger/pagesparserlogentry.cpp
+++ src/parser/logger/pagesparserlogentry.cpp
@@ -1,19 +1,18 @@
 // Flamingo is an open-source cross platform program for learning languages
 // Copyright (C) 2017 Sergey Zakharov
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 
 #include "pagesparserlogentry.h"
 
@@ -21,6 +20,7 @@ namespace parser {
 
 PagesParserLogEntry::PagesParserLogEntry(LOG_ENTRY_TYPE type, const QString& message, const QString& path, long lineNumber, const QString& line, PagesParser::SECTION section)
     : ParserLogEntry(type, message, path, lineNumber, line)
+    , section(section)
 {
     QString resultMessage = QString("(PagesParser) ") + this->message;
     switch (section) {
